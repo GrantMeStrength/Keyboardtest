be5b224d9dba9572522209d824adc37a54c1afe2
John Kennedy <john_kenn@mac.com>
2024-06-17 15:50:42 -0700

    Initial Commit

---
+++ KeyboardTest/KeyboardTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardTestApp.swift
+//  KeyboardTest
+//
+//  Created by John Kennedy on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
